77ae1a29006898328f1c6229e9049ee115d1e9cf
ArielOnoriaga <arielonoriaga2015@gmail.com>
2021-04-08 23:54:46 -0300

    template sintax

--- CustomElements/myElement.js
+++ CustomElements/myElement.js
@@ -1,27 +1,45 @@
-const template = document.createElement('div');
-template.innerHTML = `
-    <style>
-        button {
-            background: #1f1f1f;
-            font-size: 1rem;
-            color: white;
-            border-radius: 10px;
-            padding: 10px;
-        }
-    </style>
-
-    <button>
-        Hola Mundo de WebComponents
-    </button>
-`
-
 class myElement extends HTMLElement {
     constructor() {
         super();
     }
 
+    getTemplate() {
+        const template = document.createElement('template');
+        template.innerHTML = `
+            <section>
+                <h2>
+                    Hola Template Sintax
+                </h2>
+
+                <button>
+                    Template Button
+                </button>
+            </section>
+            ${this.getStyles()}
+        `;
+
+        return template;
+    }
+
+    getStyles() {
+        return `
+        <style>
+            button {
+                padding: 10px;
+                background: black;
+                color: white;
+                border-radius: 10px;
+            }
+        </style>
+        `
+    }
+
+    render() {
+        this.appendChild(this.getTemplate().content.cloneNode(true));
+    }
+
     connectedCallback() {
-        this.appendChild(template);
+        this.render();
     }
 }
 
